9edbf61099fb402ca97797f7ddc138b175eb8553
Kaniadzic <szymon.kania@edu.uekat.pl>
2022-11-22 16:58:23 +0100

    Task: lab 10

--- main.py
+++ main.py
@@ -23,10 +23,32 @@ class ImageProcessing:
         return self.images
 
     def process_image(self, image_path: str):
-        # img = cv.imread(image_path)
+
         img = cv.imread(f'{self.path}/{image_path}')
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        # converted_img = cv.threshold(
+        #     cv.medianBlur(img, 3), 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU
+        # )[1]
+
+        converted_img = cv.threshold(
+            cv.bilateralFilter(img, 5, 75, 75), 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU
+        )[1]
+
+        # converted_img = cv.threshold(
+        #     cv.GaussianBlur(img, (5, 5), 0), 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU
+        # )[1]
+
+        # converted_img = cv.threshold(
+        #     cv.medianBlur(img, 3), 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 32
+        # )[1]
+
+        cv.imshow('image', converted_img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
         print(f'Przetwarzenie obrazka {image_path}...')
-        text = pytesseract.image_to_string(img)
+        text = pytesseract.image_to_string(converted_img)
         print(text)
 
 
